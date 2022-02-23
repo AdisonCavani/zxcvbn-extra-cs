13a9e13b8cca58fdf5339720eecb129afca547f1
Adrian Środoń <adriansrodon@tuta.io>
2022-02-23 13:41:59 +0100

    Delete upload-results.ps1

--- upload-results.ps1
+++
@@ -1,4 +0,0 @@
-# upload results to Coveralls
-
- $result = Get-ChildItem -Path . -Filter coverage.cobertura.xml -Recurse |%{$_.FullName}
- .\tools\coveralls.io\tools\coveralls.net.exe --autodetect $result
