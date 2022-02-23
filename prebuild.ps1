76a00c5f988fe296baa93a4952f4ca57a9a2d2bb
Adrian Środoń <adriansrodon@tuta.io>
2022-02-23 13:41:47 +0100

    Delete prebuild.ps1

--- prebuild.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Patching Project with commit message..."
-
-(Get-Content .\zxcvbn-core\zxcvbn-core.csproj).replace('$commitMsg', $Env:APPVEYOR_REPO_COMMIT_MESSAGE) | Set-Content .\zxcvbn-core\zxcvbn-core.csproj
-
